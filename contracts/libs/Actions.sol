7ff0bac8e98e4d0f771e0a91ed22e5a5b876996b
eqfwcev123 <dohyeonee95@hotmail.com>
2022-08-04 01:04:27 +0800

    File rearrangement

--- contracts/libs/Actions.sol
+++ contracts/libs/Actions.sol
@@ -17,6 +17,11 @@ library Actions {
         ReRollOption
     }
 
+    enum OptionType {
+        CALL,
+        PUT
+    }
+
     struct ActionArgs {
         ////@notice: Type of action to execute
         ActionType actionType;
@@ -30,6 +35,8 @@ library Actions {
         uint256 amount;
     }
 
+    //// @dev : Includes all the option arguments that will be passed from outside
+    ////@dev : Have to be discussed on whether an option reroll can be cancelled when borrower returns the borrowed amount
     struct OptionActionArgs {
         ////@notice: Type of option action to execute
         OptionActionType optionActionType;
@@ -43,10 +50,55 @@ library Actions {
         uint256 putOptionPurchaseAmount;
         ////@notice: Option expiration date
         uint256 expiration;
+        ////@notice: How many times to reroll the option
+        uint256 optionRerollCount;
         ////@notice: Check option has been exercised
         bool exercised;
         ////@notice: Address of the borrower
         address borrowerAddress;
+    }
+
+    struct BuyOptionArgs {
+        ////@notice: Option type : [CALL | PUT]
+        OptionType optionType;
+        ////@notice: Option Stirke price
+        uint256 strikePrice;
+        ////@notice: Option Premium 
+        uint256 optionPremium;
+        ////@notice: Amount of [CALL | PUT] option to purchase
+        uint256 optionBuyAmount;
+        ////@notice: Option expiration date
+        uint256 expiration;
+        ////@notice: Check option has been exercised
+        bool exercised;
+        ////@notice: Address of the borrower
+        address borrowerAddress;
+    }
+
+    //// @dev : TODO For future option products (TBD)
+    struct SellOptionArgs {
+        ////@notice: Option type : [Call | PUT]
+        OptionType optionType;
+        ////@notice: Option Stirke price
+        uint256 strikePrice;
+        ////@notice: Option collateral which option seller has to pay. 0 if not option seller
+        uint256 optionCollateral;
+        ////@notice: Amount of [CALL | PUT] option to sell
+        uint256 optionSellAmount;
+        ////@notice: Option expiration date
+        uint256 expiration;
+        ////@notice: Check option has been exercised
+        bool exercised;
+        ////@notice: Address of the borrower
+        address borrowerAddress;
+    }
+
+    // struct RedeemRewardArgs {} 
+
+    // struct RerollArgs {}
+
+    function _setBuyOptionArgs(OptionActionArgs memory _args) internal pure returns (BuyOptionArgs memory){
+        require(_args.optionActionType == OptionActionType.BuyOption);
     }
 
 }
